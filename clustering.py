116efd0dc854d75f9d1211262b4eb5b5e7f121c7
edwardmp <e.poot@evolutionhost.nl>
2015-06-11 23:36:07 +0200

    Initial commit

--- clustering.py
+++ clustering.py
@@ -1,6 +1,9 @@
 import logging 
-from gensim import corpora, models, similarities, matutils
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import csv
+from gensim import corpora, models, similarities, matutils
 from scipy import stats
 from sklearn.cluster import AgglomerativeClustering
 from sklearn import metrics
@@ -8,9 +11,6 @@ from sklearn.pipeline import Pipeline
 from math import log
 from nltk.corpus import stopwords
 from sklearn.metrics import silhouette_samples, silhouette_score
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import csv
 from sklearn.metrics import jaccard_similarity_score
 
 import matplotlib.pyplot as plt
@@ -38,30 +38,25 @@ def openInputDataFileAndReturnSentencesRows(fileName='input/data.csv'):
 
     return (jobs, jobIds)
 
-sentenceList, jobIds = openInputDataFileAndReturnSentencesRows()
+def writeClustersForEachJobIdToOutputFile(clusterLabels, allJobIds, sentenceList, fileName="output/outputClusterForJobId.csv"):
+    with open(fileName, "w") as file:
+        writer = csv.writer(file, delimiter=",")
+        writer.writerow(["JobId", "Cluster", "Sentence"])
+     
+        count = 0
+        for sentence in sentenceList:
+            clusterNumber = clusterLabels[count]
+            writer.writerow([jobIds[count], clusterNumber, sentence])
+            count += 1
 
-bigram = models.Phrases([line.lower().split() for line in sentenceList])
-
-stopWords = loadStopWords()
-
-dictionary = corpora.Dictionary(bigram[line.lower().split()] for line in sentenceList)
-
-stop_ids = [dictionary.token2id[stopword] for stopword in stopWords
-          if stopword in dictionary.token2id]
-
-dictionary.filter_tokens(stop_ids)
-dictionary.compactify()
-print(dictionary)
-
-bagOfWords = [dictionary.doc2bow(bigram[line.lower().split()]) for line in sentenceList]
-
-tfidf = models.TfidfModel(bagOfWords)
-corpus_tfidf = tfidf[bagOfWords]
-lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=30)
-corpus_lsi = lsi[corpus_tfidf]
-
-mycorpus_matrix = matutils.corpus2dense(corpus_lsi, num_terms=30).transpose()
-
+def writeInfoForEachJobIdComboToOutputFile(jobCombinationAndCoefficients, fileName="output/outputInfoForJobIdCombo.csv"):
+    with open(fileName, "w+") as file:
+        writer = csv.writer(file, delimiter=",")
+        writer.writerow(["JobId_First", "JobId_Second", "Jaccard_Similarity_Score"])
+     
+        for job in jobCombinationAndCoefficients:
+            writer.writerow([job["job_id_first"], job["job_id_second"], job["jaccard_similarity_score"]])
+            
 def calculateNumberOfIdealClusters(maxAmount, corpus):
     range_n_clusters = range(2, maxAmount) # max amount of clusters equal to amount of jobs
 
@@ -90,88 +85,97 @@ def calculateNumberOfIdealClusters(maxAmount, corpus):
     print ("Highest score = %f for n_clusters = %d" % (silhouette_high, silhouette_high_n_clusters))
     return silhouette_high_n_clusters
 
-numClusters = 3 #calculateNumberOfIdealClusters(40, mycorpus_matrix)
+
+def calculate_amount_of_clusters_for_each_job_id(clusterLabels, jobIds):
+    dictionaryWithClustersForEachJobId = {}
+
+    for index, value in enumerate(clusterLabels):
+         jobIdForSentence = jobIds[index]
+
+         if jobIdForSentence not in dictionaryWithClustersForEachJobId:
+            dictionaryWithClustersForEachJobId[jobIdForSentence] = {}
+
+         jobIdDict = dictionaryWithClustersForEachJobId[jobIdForSentence]
+
+         if value not in jobIdDict:
+            num = 0
+         else:
+            num = jobIdDict[value]
+
+         jobIdDict[value] = num + 1
+
+    return dictionaryWithClustersForEachJobId
+
+def calculateJaccardScoreForEachJobCombo(allJobIds, clustersForEachjobSentence, clusterLabels):
+    jobCombinationAndCoefficients = []
+    index = 0;
+    for jobId in list(set(jobIds)):
+        copy_list = list(set(jobIds[:])) # only unique jobIds, after that convert back to list
+        del copy_list[index] #remove self
+
+        for eachOtherJobId in copy_list:
+            otherJobDict = clustersForEachjobSentence[eachOtherJobId]
+            currentJobDict = clustersForEachjobSentence[jobId]
+
+            for key in set(clusterLabels):
+                if key not in currentJobDict:
+                    currentJobDict[key] = 0
+
+                if key not in otherJobDict:
+                    otherJobDict[key] = 0
+
+            coefficient = jaccard_similarity_score(currentJobDict.values(), otherJobDict.values())
+            jobCombinationAndCoefficients.append({"job_id_first": jobId, "job_id_second": eachOtherJobId, "jaccard_similarity_score": coefficient})
+
+            if (coefficient >= 0.00):
+                print "Job with ids %s and %s are %f similar %s %s" % (jobId, eachOtherJobId, coefficient, currentJobDict, otherJobDict)
+
+        index += 1
+
+    return jobCombinationAndCoefficients
+
+sentenceList, jobIds = openInputDataFileAndReturnSentencesRows()
+
+# Automatically detect common phrases, bigram words are underscored in between them (frequently co-occurring tokens)
+bigram = models.Phrases([line.lower().split() for line in sentenceList])
+
+# Load a list of stopwords
+stopWords = loadStopWords()
+
+# convert tokens to vector
+bigramAsDictionary = corpora.Dictionary(bigram[[line.split() for line in sentenceList]])
+
+# convert stopwords to id
+stopWordIds = [bigramAsDictionary.token2id[stopword] for stopword in stopWords
+          if stopword in bigramAsDictionary.token2id]
+
+# remove stopwords
+bigramAsDictionary.filter_tokens(stopWordIds)
+bigramAsDictionary.compactify()
+
+# convert to (word_id, word_frequency) tuples
+bagOfWords = [bigramAsDictionary.doc2bow(bigram[line.split()]) for line in sentenceList]
+
+# convert matrix to term frequencies matrix
+tfidf = models.TfidfModel(bagOfWords)
+corpusTfidf = tfidf[bagOfWords]
+
+# use LSA analysis on corpus
+lsi = models.LsiModel(corpusTfidf, id2word=bigramAsDictionary, num_topics=30)
+corpusLsi = lsi[corpusTfidf]
+
+# convert corpus to array
+corpusAsMatrix = matutils.corpus2dense(corpusLsi, num_terms=30).transpose()
+
+# calculate ideal number of clusters based on silhouette analysis
+numClusters = calculateNumberOfIdealClusters(len(set(jobIds)), corpusAsMatrix)
 
 cluster = AgglomerativeClustering(n_clusters=numClusters, linkage="ward", affinity="euclidean")
-cluster_labels = cluster.fit_predict(mycorpus_matrix)
+clusterLabels = cluster.fit_predict(corpusAsMatrix)
 
-print len(cluster_labels)
+clustersForEachjobSentence = calculate_amount_of_clusters_for_each_job_id(clusterLabels, jobIds)
 
-print len(jobIds)
-
-def calculate_amount_of_clusters_for_each_job_id(cluster_labels, jobIds):
-
-	dictionaryWithClustersForEachJobId = {}
-
-	for index, value in enumerate(cluster_labels):
-		 jobIdForSentence = jobIds[index]
-
-		 if jobIdForSentence not in dictionaryWithClustersForEachJobId:
-		 	dictionaryWithClustersForEachJobId[jobIdForSentence] = {}
-
-		 jobIdDict = dictionaryWithClustersForEachJobId[jobIdForSentence]
-
-		 if value not in jobIdDict:
-		 	num = 0
-		 else:
-		 	num = jobIdDict[value]
-
-		 jobIdDict[value] = num + 1
-
-	return dictionaryWithClustersForEachJobId
-
-clustersForEachjobSentence = calculate_amount_of_clusters_for_each_job_id(cluster_labels, jobIds)
-
-def writeClustersForEachJobIdToOutputFile(cluster_labels, allJobIds, fileName="output/outputClusterForJobId.csv"):
-    with open(fileName, "w") as file:
-        writer = csv.writer(file, delimiter=",")
-        writer.writerow(["JobId", "Cluster"])
-     
-        count = 0
-        for job_id in allJobIds:
-            clusterNumber = cluster_labels[count]
-            writer.writerow([job_id, clusterNumber])
-            count += 1
-
-def calculateJaccardScoreForEachJobCombo(allJobIds, clustersForEachjobSentence):
-	jobCombinationAndCoefficients = []
-	index = 0;
-	for jobId in list(set(jobIds)):
-		copy_list = list(set(jobIds[:])) # only unique jobIds, after that convert back to list
-		del copy_list[index] #remove self
-
-		for eachOtherJobId in copy_list:
-			otherJobDict = clustersForEachjobSentence[eachOtherJobId]
-			currentJobDict = clustersForEachjobSentence[jobId]
-
-			for key in set(cluster_labels):
-				if key not in currentJobDict:
-					currentJobDict[key] = 0
-
-				if key not in otherJobDict:
-				 	otherJobDict[key] = 0
-
-			coefficient = jaccard_similarity_score(currentJobDict.values(), otherJobDict.values())
-			jobCombinationAndCoefficients.append({"job_id_first": jobId, "job_id_second": eachOtherJobId, "jaccard_similarity_score": coefficient})
-
-			if (coefficient >= 0.00):
-				print "Job with ids %s and %s are %f similar %s %s" % (jobId, eachOtherJobId, coefficient, currentJobDict, otherJobDict)
-
-		index += 1
-
-	return jobCombinationAndCoefficients
-
-jobCombinationAndCoefficients = calculateJaccardScoreForEachJobCombo(jobIds, clustersForEachjobSentence)
-
-def writeInfoForEachJobIdComboToOutputFile(jobCombinationAndCoefficients, fileName="output/outputInfoForJobIdCombo.csv"):
-    with open(fileName, "w+") as file:
-        writer = csv.writer(file, delimiter=",")
-        writer.writerow(["JobId_First", "JobId_Second", "Jaccard_Similarity_Score"])
-     
-        for job in jobCombinationAndCoefficients:
-            writer.writerow([job["job_id_first"], job["job_id_second"], job["jaccard_similarity_score"]])
+jobCombinationAndCoefficients = calculateJaccardScoreForEachJobCombo(jobIds, clustersForEachjobSentence, clusterLabels)
 
 writeInfoForEachJobIdComboToOutputFile(jobCombinationAndCoefficients)
-writeClustersForEachJobIdToOutputFile(cluster_labels, jobIds)
-
-
+writeClustersForEachJobIdToOutputFile(clusterLabels, jobIds, sentenceList)
